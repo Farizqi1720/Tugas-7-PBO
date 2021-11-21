977778e1ee2e62c0c19ca2aea755ede7c5853c60
Farizqi1720 <92361454+Farizqi1720@users.noreply.github.com>
2021-11-21 13:36:29 +0700

    Add files via upload

---
+++ storage/framework/views/63b35bfdc44c3b4bc7f457887117d1cb403c6713.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html>
+
+	<head>
+
+    <title>TABEL CRUD MAHASISWA</title>
+
+		<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+
+	</head>
+
+	<body>
+	
+		<div class="container">
+
+			<?php echo $__env->yieldContent('content'); ?>
+
+		</div>
+   
+	</body>
+
+</html><?php /**PATH F:\XAMPP\htdocs\enyar\resources\views/mahasiswa/layout.blade.php ENDPATH**/ ?>
